54b90dfe2a17afeecc0bf5db680ec27f902a0d50
unknown <rajesh.ece12@yahoo.com>
2019-02-16 17:59:38 +0530

    Initail commit

---
+++ JavaWithReactPMT/src/main/java/com/java/react/JavaWithReactPmtApplication.java
@@ -0,0 +1,14 @@
+package com.java.react;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaWithReactPmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaWithReactPmtApplication.class, args);
+	}
+
+}
+
