39328b58578e3c61ac56373f7d7c6831852960ec
unknown <rajesh.ece12@yahoo.com>
2019-02-17 12:50:04 +0530

    further update in API

---
+++ JavaWithReactPMT/src/main/java/com/java/react/exception/CustomeExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.java.react.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+@RestController
+public class CustomeExceptionHandler extends ResponseEntityExceptionHandler{
+	
+	@ExceptionHandler
+	public final ResponseEntity<Object> handleProjectIdException(ProjectIdException ex, WebRequest req){
+		
+		ProjectIdExceptionResponse exceptionResponse = new ProjectIdExceptionResponse(ex.getMessage());
+		return new ResponseEntity<Object>(exceptionResponse, HttpStatus.BAD_REQUEST);
+		
+	}
+
+}
